fa1cb495956ce75299206ae5c03e4a6b924a6d47
Nguyen Tien Thanh <thanhnthe163528@fpt.edu.vn>
2024-03-27 13:27:01 +0700

    feat: Migrate from @next/font to built-in next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-import localFont from "@next/font/local";
+import localFont from "next/font/local";
 
 const circular = localFont({
   src: [

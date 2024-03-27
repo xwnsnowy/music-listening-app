5706643cf4467aefdf031c742c3f4cbce4b350c2
Nguyen Tien Thanh <thanhnthe163528@fpt.edu.vn>
2024-03-27 13:18:57 +0700

    feat: Add Header Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 import Siderbar from "@/components/Siderbar";
 
 export default function Home() {
@@ -6,7 +8,20 @@ export default function Home() {
       <Siderbar />
       <main className="h-full flex-1 overflow-y-auto px-1 py-2">
         <div className="h-full w-full overflow-hidden overflow-y-auto rounded-lg bg-bgBase">
-          {/* Header */}
+          <Header>
+            <div className="mb-2">
+              <h1 className="font-circular text-3xl font-semibold text-primaryColor">
+                Welcome Back !
+              </h1>
+            </div>
+            <div className="mt-4 grid grid-cols-1 gap-3 text-primaryColor sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+              <ListItem
+                image="/images/liked.png"
+                name="Liked Song"
+                href="liked"
+              />
+            </div>
+          </Header>
           <div className="mb-7 mt-2 px-6 font-circular text-2xl font-semibold text-primaryColor">
             <h1>Newest Songs</h1>
           </div>
---
+++ src/components/Header.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { BsFillSearchHeartFill } from "react-icons/bs";
+import { HiHome } from "react-icons/hi";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { twMerge } from "tailwind-merge";
+import Button from "./Button";
+import Link from "next/link";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    //Handle logout
+  };
+
+  return (
+    <div
+      className={twMerge(
+        `h-fit bg-gradient-to-b from-slate-700 p-6`,
+        className
+      )}
+    >
+      <div className="mb-4 flex w-full items-center justify-between">
+        <div className="hidden items-center gap-x-2 md:flex">
+          <button
+            onClick={() => router.back()}
+            title="Arrow Left"
+            className="flex items-center justify-center rounded-full bg-black transition hover:opacity-75"
+          >
+            <RxCaretLeft size={35} className="text-primaryColor" />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            title="Arrow Left"
+            className="flex items-center justify-center rounded-full bg-black transition hover:opacity-75"
+          >
+            <RxCaretRight size={35} className="text-primaryColor" />
+          </button>
+        </div>
+        {/* Icon Home Mobile */}
+        <div className="flex items-center gap-x-2 md:hidden">
+          <button
+            title="Home Mobile"
+            className="flex items-center justify-center rounded-full bg-white p-2 transition hover:opacity-75"
+          >
+            <HiHome size={20} className="text-black" />
+          </button>
+          <button
+            title="Search Mobile"
+            className="flex items-center justify-center rounded-full bg-white p-2 transition hover:opacity-75"
+          >
+            <BsFillSearchHeartFill size={20} className="text-black" />
+          </button>
+        </div>
+        <div className="flex items-center justify-between gap-x-4">
+          <>
+            <div>
+              <Link href="/signup">
+                <Button className="transform bg-transparent px-4 text-primaryColor hover:scale-110">
+                  Sign Up
+                </Button>
+              </Link>
+            </div>
+            <div>
+              <Link href="/login">
+                <Button className="transform px-10 py-3 hover:scale-110">
+                  Log In
+                </Button>
+              </Link>
+            </div>
+          </>
+        </div>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default Header;
